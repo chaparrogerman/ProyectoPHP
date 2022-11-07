1ec1969d3446c55637d5ba5624c42b2c1f6322d3
chaparrogerman <110686344+chaparrogerman@users.noreply.github.com>
2022-11-06 22:29:55 -0300

    Update script.js
    ultima version

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const correo = document.getElementById("correo")
+const cantidad = document.getElementById("cantidad")
+const categoria = document.getElementById("categoria")
+
 function borrar(){
     document.getElementById("total").innerHTML = "Total a Pagar: $"
 }
@@ -5,10 +11,10 @@ function borrar(){
 function validar(nombre,apellido,correo,cantidad){
     var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     var esValido= expReg.test(correo)
-    if(nombre==''){
-        alert("Ingrese un nombre")
-    }else if(apellido==''){
-        alert("Ingrese un apellido")
+    if(nombre=='' || nombre.length <3){
+        alert("Ingrese un nombre valido")
+    }else if(apellido=='' || apellido.length <3){
+        alert("Ingrese un apellido valido")
     }else if(esValido==false){
         alert("El correo ingresado no es valido")
     }else if(!Number.isInteger(cantidad) || cantidad==0){
@@ -20,21 +26,17 @@ function validar(nombre,apellido,correo,cantidad){
 }
 
 function precio(cantidad){
-    let categoria = parseInt(document.getElementById("categoria").value)
     let total = 0
-    switch(categoria){
+    switch(parseInt(categoria.value)){
         case 1:
-            console.log("la categoria es"+categoria)
             total = cantidad * (200 - 200 * 0.8)
             document.getElementById("total").innerHTML = "Total a Pagar: $"+total
             break
         case 2:
-            console.log("la categoria es"+categoria)
             total = cantidad * (200 - 200 * 0.5)
             document.getElementById("total").innerHTML = "Total a Pagar: $"+total
             break
         case 3:
-            console.log("la categoria es"+categoria)
             total = cantidad * (200 - 200 * 0.15)
             document.getElementById("total").innerHTML = "Total a Pagar: $"+total
             break
@@ -42,9 +44,5 @@ function precio(cantidad){
 }
 
 function totalPagar(){
-    let nombre = document.getElementById("nombre").value
-    let apellido = document.getElementById("apellido").value
-    let correo = document.getElementById("correo").value
-    let cantidad = parseInt(document.getElementById("cantidad").value)
-    validar(nombre, apellido, correo, cantidad)
+    validar(nombre.value, apellido.value, correo.value, parseInt(cantidad.value))
 }
